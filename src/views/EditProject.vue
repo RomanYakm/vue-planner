ffb1f87d463a7e9ff13862bbe20dfdd6c52b5c12
Roman Yakymiuk <romeronice93@gmail.com>
2023-02-28 15:42:31 +0200

    first commit

---
+++ src/views/EditProject.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>Edit Project</h1>
+  <form @submit.prevent="handleEdit">
+    <label>Title:</label>
+    <input type="text" v-model="title" required />
+    <label>Details:</label>
+    <textarea required v-model="details"></textarea>
+    <button>Update Project</button>
+  </form>
+</template>
+
+<script>
+export default {
+  props: ["id"],
+  data() {
+    return {
+      title: "",
+      details: "",
+      uri: "http://localhost:3000/projects/" + this.id,
+    };
+  },
+  methods: {
+    handleEdit() {
+      fetch(this.uri, {
+        method: "PATCH",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: this.title, details: this.details }),
+      })
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch((err) => console.log(err.message));
+    },
+  },
+  mounted() {
+    fetch(this.uri)
+      .then((res) => res.json())
+      .then(({ title, details }) => {
+        (this.title = title), (this.details = details);
+      })
+      .catch((err) => console.log(err.message));
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
